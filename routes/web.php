1cbba3a607483e09990d1edffcc70bf18e3efdba
ahmad adnan <120600994+ahmadadnanes@users.noreply.github.com>
2025-02-25 18:02:49 +0300

    done

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth' , 'admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
